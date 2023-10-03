8bdc457bcd9e79f045ce1643c1087300c7f5840c
Yukino140 <mohamedomarfitouri@gmail.com>
2023-10-03 22:13:11 +0100

    TP Symfony

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    /**
+     * @Route("/service", name="app_service")
+     */
+    public function index(): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+
+     /**
+     * @Route("/serv/{name}", name="serv")
+     */
+    public function showService($name):Response
+    {
+        return $this->render('service/showService.html.twig',[
+            'n'=>$name
+        ]);
+    }
+}
